20661b451446ced0f3c4307b9e5c799af516f546
Alan Cardero <alan.cardero@gmail.com>
2018-06-16 11:23:06 -0400

    added book to add forms

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router  = express.Router();
 const Book = require('../models/book.js');
 
 
+router.get('/books/add', (req, res, next) => {
+  res.render("book-add")
+});
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -28,6 +32,18 @@ router.get('/book/:id', (req, res, next) => {
     .catch(error => {
       console.log(error)
     })
+});
+
+router.post('/books/add', (req, res, next) => {
+  const { title, author, description, rating } = req.body;
+  const newBook = new Book({ title, author, description, rating})
+  newBook.save()
+  .then((book) => {
+    res.redirect('/books')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
 });
 
 module.exports = router;
