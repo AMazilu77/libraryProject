fa11051f7f863faf18fd13932da4fd76e8d77fb8
Alan Cardero <alan.cardero@gmail.com>
2018-06-16 15:49:54 -0400

    project fully working

--- models/book.js
+++ models/book.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 const bookSchema = new Schema({
   title: String,
   description: String,
-  author: String,
-  rating: Number
+  author: [ { type : ObjectId, ref: 'Author' } ],
+  rating: Number,
+  reviews: [ 
+    {
+      user: String,
+      comments: String
+    } 
+  ]
 }, {
   timestamps: {
     createdAt: "created_at",
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Book = require('../models/book.js');
-
+const Author = require('../models/author.js');
 
 router.get('/books/add', (req, res, next) => {
   res.render("book-add")
@@ -25,13 +25,18 @@ router.get('/books', (req, res, next) => {
 
 router.get('/book/:id', (req, res, next) => {
   let bookId = req.params.id;
+  if (!/^[0-9a-fA-F]{24}$/.test(bookId)) { 
+    return res.status(404).render('not-found');
+  }
   Book.findOne({'_id': bookId})
+    .populate('author')
     .then(book => {
+      if (!book) {
+          return res.status(404).render('not-found');
+      }
       res.render("book-detail", { book })
     })
-    .catch(error => {
-      console.log(error)
-    })
+    .catch(next)
 });
 
 router.post('/books/add', (req, res, next) => {
@@ -61,6 +66,33 @@ router.post('/books/edit', (req, res, next) => {
   Book.update({_id: req.query.book_id}, { $set: {title, author, description, rating }}, { new: true })
   .then((book) => {
     res.redirect('/books')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
+
+router.get('/authors/add', (req, res, next) => {
+  res.render("author-add")
+});
+
+router.post('/authors/add', (req, res, next) => {
+  const { name, lastName, nationality, birthday, pictureUrl } = req.body;
+  const newAuthor = new Author({ name, lastName, nationality, birthday, pictureUrl})
+  newAuthor.save()
+  .then((book) => {
+    res.redirect('/books')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
+
+router.post('/reviews/add', (req, res, next) => {
+  const { user, comments } = req.body;
+  Book.update({ _id: req.query.book_id }, { $push: { reviews: { user, comments }}})
+  .then(book => {
+    res.redirect('/book/' + req.query.book_id)
   })
   .catch((error) => {
     console.log(error)
